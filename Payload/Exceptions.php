d06c37db3397888e1d176cf470c587965e3b50ea
Ian Halpern <ian@payload.co>
2020-01-10 10:56:02 -0500

    Removed extra forward slash

--- Payload/Exceptions.php
+++ Payload/Exceptions.php
@@ -1,12 +1,12 @@
 <?php
-require(dirname(__FILE__) . '//Exceptions/PayloadError.php');
-require(dirname(__FILE__) . '//Exceptions/BadRequest.php');
-require(dirname(__FILE__) . '//Exceptions/Forbidden.php');
-require(dirname(__FILE__) . '//Exceptions/InternalServerError.php');
-require(dirname(__FILE__) . '//Exceptions/InvalidAttributes.php');
-require(dirname(__FILE__) . '//Exceptions/NotFound.php');
-require(dirname(__FILE__) . '//Exceptions/ServiceUnavailable.php');
-require(dirname(__FILE__) . '//Exceptions/TooManyRequests.php');
-require(dirname(__FILE__) . '//Exceptions/Unauthorized.php');
-require(dirname(__FILE__) . '//Exceptions/UnknownResponse.php');
+require(dirname(__FILE__) . '/Exceptions/PayloadError.php');
+require(dirname(__FILE__) . '/Exceptions/BadRequest.php');
+require(dirname(__FILE__) . '/Exceptions/Forbidden.php');
+require(dirname(__FILE__) . '/Exceptions/InternalServerError.php');
+require(dirname(__FILE__) . '/Exceptions/InvalidAttributes.php');
+require(dirname(__FILE__) . '/Exceptions/NotFound.php');
+require(dirname(__FILE__) . '/Exceptions/ServiceUnavailable.php');
+require(dirname(__FILE__) . '/Exceptions/TooManyRequests.php');
+require(dirname(__FILE__) . '/Exceptions/Unauthorized.php');
+require(dirname(__FILE__) . '/Exceptions/UnknownResponse.php');
 ?>
